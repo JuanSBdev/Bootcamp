803a37a797c1fcda87a25ac03b2651e53649a5ca
Juan SB <juansb.dev@gmail.com>
2022-10-05 02:42:06 -0300

    tareas

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -167,7 +167,7 @@ function empiezaConNueve(n) {
 
 }
 
-}
+
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
