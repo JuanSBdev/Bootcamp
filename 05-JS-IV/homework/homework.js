42fd55b9ed0f6c48449395ea40182639c7f94c40
Juan SB <juansb.dev@gmail.com>
2022-10-27 01:15:41 -0300

    casi

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -135,6 +135,12 @@ function sumarLikesDeUsuario (usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  
+  let suma = 0;
+  for (let i = 0; i < usuario.posts.length; i++) {
+    suma = suma + usuario.posts[i].likes;
+  }
+  return suma;
 }
 
 function agregarMetodoCalculoDescuento (producto) {
